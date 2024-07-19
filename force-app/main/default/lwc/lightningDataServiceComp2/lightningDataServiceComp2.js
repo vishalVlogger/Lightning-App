0ee05394391b9e4f8c9476eaa3d940d79322e5d2
Vishal <vishal.patil362000@gmail.com>
2024-07-19 15:33:58 +0530

    GetPicklistValuesByRecordType

--- force-app/main/default/lwc/lightningDataServiceComp2/lightningDataServiceComp2.js
+++ force-app/main/default/lwc/lightningDataServiceComp2/lightningDataServiceComp2.js
@@ -1,20 +1,31 @@
-import { getObjectInfo } from "lightning/uiObjectInfoApi";
+import {
+  getObjectInfo,
+  getPicklistValues,
+  getPicklistValuesByRecordType
+} from "lightning/uiObjectInfoApi";
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
+import ACCOUNT_INDUSTRY from "@salesforce/schema/Account.Industry";
 import { LightningElement, wire } from "lwc";
 
 export default class LightningDataServiceComp2 extends LightningElement {
-  accountInfo;
-  errors;
+  value;
 
   @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
-  wireGetObjectInfo({ error, data }) {
-    if (data) {
-      console.log("Data: ", data);
-      this.accountInfo = data;
-      this.errors = null;
-    } else if (error) {
-      this.errors = error;
-      this.accountInfo = null;
-    }
+  wireObjectInfo;
+
+  @wire(getPicklistValues, {
+    recordTypeId: "$wireObjectInfo.data.defaultRecordTypeId",
+    fieldApiName: ACCOUNT_INDUSTRY
+  })
+  wireGetIndustry;
+
+  @wire(getPicklistValuesByRecordType, {
+    objectApiName: ACCOUNT_OBJECT,
+    recordTypeId: "$wireObjectInfo.data.defaultRecordTypeId"
+  })
+  wireGetAccPickList;
+
+  handleChange(event) {
+    this.value = event.target.value;
   }
 }
