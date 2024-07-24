4a2fe778a1dbbbd5c519ba49ba1dce03b9060e11
Vishal <vishal.patil362000@gmail.com>
2024-07-24 11:02:35 +0530

    LMS

---
+++ force-app/main/default/lwc/subscribLMS/subscribLMS.js
@@ -0,0 +1,46 @@
+import { LightningElement, wire } from "lwc";
+import {
+  subscribe,
+  unsubscribe,
+  APPLICATION_SCOPE,
+  MessageContext
+} from "lightning/messageService";
+import recordSelected from "@salesforce/messageChannel/sendMessage__c";
+
+export default class SubscribLMS extends LightningElement {
+  subscription = null;
+  pubMessage = "";
+
+  @wire(MessageContext)
+  messageContext;
+
+  subscribeToMessageChannel() {
+    if (!this.subscription) {
+      this.subscription = subscribe(
+        this.messageContext,
+        recordSelected,
+        (message) => this.handleMessage(message),
+        { scope: APPLICATION_SCOPE }
+      );
+    }
+  }
+
+  unsubscribeToMessageChannel() {
+    unsubscribe(this.subscription);
+    this.subscription = null;
+  }
+
+  // Handler for message received by component
+  handleMessage(message) {
+    this.pubMessage = message.lmsData;
+  }
+
+  // Standard lifecycle hooks used to subscribe and unsubsubscribe to the message channel
+  connectedCallback() {
+    this.subscribeToMessageChannel();
+  }
+
+  disconnectedCallback() {
+    this.unsubscribeToMessageChannel();
+  }
+}
